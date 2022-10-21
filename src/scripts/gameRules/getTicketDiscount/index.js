ffcdba1b3c9105288995945cacac581f6c5fdccc
Marcos Maia <henriquemarcosmaia@gmail.com>
2022-10-21 12:16:46 -0300

    desconto

--- src/scripts/gameRules/getTicketDiscount/index.js
+++ src/scripts/gameRules/getTicketDiscount/index.js
@@ -3,5 +3,5 @@ import getUser from "../../storage/getUser/index.js";
 export const getTicketDiscount = () => {
   const user = getUser();
 
-  return Math.pow(1.65, user.character.ticketDiscount)
+  return Math.pow(1.06, user.character.ticketDiscount)
 }
